39b7d8a0fd58026dba8aaf326ae2df45427b1855
James Abrenica <jamesjirehabrenica@yahoo.com>
2024-10-11 15:12:40 +0800

    added readme file

--- fastapi-facebook-webhook/app/models.py
+++
@@ -1,9 +0,0 @@
-# models.py
-from pydantic import BaseModel, Field
-from typing import Optional
-
-class Log(BaseModel):
-    id: Optional[str] = Field(None, alias="_id")
-    sender_id: str
-    message: str
-    timestamp: str
